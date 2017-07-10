e7a6fafb5c8d06ae92b72c0c3c2d6d3836f2bf3a
NativeM <gustipart2@gmail.com>
2017-07-10 16:47:16 +0700

    Update Vaporwave.h

--- Vaporwave.h
+++ Vaporwave.h
@@ -1,1 +1,26 @@
+#ifndef VAPORWAVE_H
+#define VAPORWAVE_H
 
+#include <string>
+
+class Vaporwave
+{
+private:
+	std::wstring m_vaporStr;
+	int m_vaporStrLen;
+	
+public:
+	Vaporwave();
+	Vaporwave(std::wstring vaporStr);
+	Vaporwave(wchar_t* vaporStr);
+	
+	void operator=(const std::wstring& str);
+	void operator=(const Vaporwave& str);
+	
+	std::wstring toVaporwaveString();
+	
+	operator std::wstring();
+	operator const std::wstring() const;
+};
+
+#endif
